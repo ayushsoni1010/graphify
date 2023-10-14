9400279e526173dcd739ee5896952b286d2ce29c
Ayush Soni <ayushsoni1010.work@gmail.com>
2023-10-14 15:29:27 +0530

    chore: express app and general api setup

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-console.log("Starting");
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+// Middleware setup
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+// Routes
+app.get("/api", (_req, res) => {
+  res.json({ message: "Hey there!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
