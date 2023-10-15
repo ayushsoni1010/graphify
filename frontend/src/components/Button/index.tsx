96d416dfcc25aaa5781bff4a7acea517effa4b65
Ayush Soni <ayushsoni1010.work@gmail.com>
2023-10-15 19:20:13 +0530

    refactor: button tag

--- frontend/src/components/Button/index.tsx
+++ frontend/src/components/Button/index.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Button: React.FunctionComponent = () => {
   return (
-    <div className="text-md shadow-sm px-24 py-3.5 text-center text-white bg-gradient-to-r from-blue-400 from-1% via-cyan-400 to-blue-500 to-70% font-black rounded-full">
+    <button className="text-md shadow-sm px-24 py-3.5 text-center text-white bg-gradient-to-r from-blue-400 from-1% via-cyan-400 to-blue-500 to-70% font-black rounded-full">
       Only Focus on Crime Graph
-    </div>
+    </button>
   );
 };
 
