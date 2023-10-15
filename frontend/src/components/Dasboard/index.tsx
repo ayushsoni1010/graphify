a7c8734acfa35b9e5a01ae08c408a1c9d8409b8e
Ayush Soni <ayushsoni1010.work@gmail.com>
2023-10-15 12:46:18 +0530

    chore(wip): dashboard ui

---
+++ frontend/src/components/Dasboard/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "../Header";
+import BaseCard from "../Card";
+import Footer from "../Footer";
+import ChartCanvas from "../ChartCanvas";
+
+const Dashboard: React.FunctionComponent = () => {
+  return (
+    <div className="flex-col justify-center align-middle items-center px-10 py-10">
+      <Header />
+      <BaseCard />
+      <BaseCard />
+      <BaseCard outerheight="h-20" innerHeight="h-56" />
+      <ChartCanvas />
+      <Footer />
+    </div>
+  );
+};
+
+export default Dashboard;
